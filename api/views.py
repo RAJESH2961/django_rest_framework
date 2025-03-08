f7762d197bdfd324de311050cdd1b0e652537168
Rajesh <grajesh2906@gmail.com>
2025-03-08 10:37:17 +0530

    Fetching particular data using Primary Key

--- api/views.py
+++ api/views.py
@@ -26,4 +26,15 @@ def studentsView(request):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        
+
+@api_view(['GET'])
+def studentsDetailView(request, pk):
+    try:
+        student = Student.objects.get(pk=pk)
+    except Student.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    if request.method == 'GET':
+        serializer = StudentSerializers(student)
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
